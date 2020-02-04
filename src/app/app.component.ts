fa4262ff7177d8885b891c5d4671d9b6e2e070fc
Id <philippe.emeran@lepoles.com>
2020-02-04 09:54:42 +0100

    message de commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({ // Decorateur
+  selector: 'app-root', // comment appeler un composant <app-root></app-root>
+  templateUrl: './app.component.html', // style "visible" HTML
+  styleUrls: ['./app.component.css'] // style associé au rendu HTML du composant
 })
-export class AppComponent {
+export class AppComponent { // partie "logique"
   title = 'tour-of-heroes';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { AdditionComponent } from './addition/addition.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    AdditionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
